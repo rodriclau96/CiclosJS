af7a9c5ce1685c588e8f1874ff5af71cf3e2aa03
rodriclau96 <rodriguezclau1996@gmail.com>
2022-03-17 22:07:06 -0300

    Se agrega el trabajo

---
+++ app.js
@@ -0,0 +1,17 @@
+//for ([expresion-inicial]; [condicion]; [expresion-final])sentencia
+
+for(let i = 1; i < 6; i++) {
+    console.log("Su compra número" + i);
+}
+
+
+//Pato yo seguí los ejemplos que dio la profe para tratar de responder a la consigna, además busqué algunos tips, 
+//tengo una página de ropa y lo quise relacionar un poco con eso, además lo hice medio a las apuradas porque pensé que ya lo había entregado
+
+//var i = valorX;
+//while (i < valorY) 
+//{
+  //console.log(i);
+   //i++; } valorX se suma en 1 hasta llegar al valorY
+
+   //El wile lo pongo como ejemplo para ver si está bien 
